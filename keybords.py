793b29d7bbffb76c2876c38192fb13ff3f3787d0
Alex-Golovko <ya.golovko-alex2017@yandex.ru>
2023-01-08 16:35:50 +0400

    add btn tea

--- keybords.py
+++ keybords.py
@@ -11,17 +11,17 @@ basket_button = KeyboardButton(text='Корзина')
 start_up_menu.add(black_tea_button, green_tea_button, news_button, basket_button)
 
 black_tea_menu = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-bbrand_1_button = KeyboardButton(text='Бренд 1')
-bbrand_2_button = KeyboardButton(text='Бренд 2')
-bbrand_3_button = KeyboardButton(text='Бренд 3')
+bbrand_1_button = KeyboardButton(text='Richard Black')
+bbrand_2_button = KeyboardButton(text='Greendfield Black')
+bbrand_3_button = KeyboardButton(text='Lipton Black')
 bbrand_4_button = KeyboardButton(text='Назад')
 
 black_tea_menu.add(bbrand_1_button, bbrand_2_button, bbrand_3_button, bbrand_4_button)
 
 green_tea_menu = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-gbrand_1_button = KeyboardButton(text='Бренд 1')
-gbrand_2_button = KeyboardButton(text='Бренд 2')
-gbrand_3_button = KeyboardButton(text='Бренд 3')
+gbrand_1_button = KeyboardButton(text='Richard Green')
+gbrand_2_button = KeyboardButton(text='Greendfield Green')
+gbrand_3_button = KeyboardButton(text='Lipton Green')
 gbrand_4_button = KeyboardButton(text='Назад')
 
 green_tea_menu.add(gbrand_1_button, gbrand_2_button, gbrand_3_button, gbrand_4_button)
--- main.py
+++ main.py
@@ -1,27 +1,73 @@
+import logging
+import re
+
+from aiogram.types import CallbackQuery, inline_query
 from aiogram import Bot, Dispatcher, types, executor
 from config import API_TOKEN
 from keybords import start_up_menu, black_tea_menu, green_tea_menu
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+class ShopCart(StatesGroup):
+    choice = State()
+    add_to_cart = State()
+    order = State()
+    name = State()
+    mail = State()
+    adminchange = State()
 
-@dp.message_handler(commands=['start', 'help'])
-async def start_command(message: types.Message):
+logging.basicConfig(filename='logs.log', level=logging.INFO)
+bot = Bot(token=API_TOKEN,)
+
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+@dp.message_handler(commands=['start', 'help'], state='*')
+async def start_command(message: types.Message, state: FSMContext):
+    await state.finish()
     await message.answer('Добро пожаловать в наш чайный магазин\n'
                         'где вы сможете выбрать подходщий чай для себя.\n'
                         'Приятного чаяпития!', reply_markup=start_up_menu)
     await message.delete()
 
-@dp.message_handler(text='Назад')
-async  def back_button_commands(message: types.Message):
+@dp.message_handler(text="admin", state='*')
+async def admin_check(message: types.Message):
+    user_id = message.chat.id
+    admin_ind = False
+    first_admin = 498618666
+    print(user_id)
+    if user_id == first_admin:
+        print("Nice")
+        await message.reply(text="darova admin") #, reply_markup=admin_start_markup()
+        admin_ind = True
+    else:
+        print("fuckoff")
+    print(message.chat.id)
+
+@dp.message_handler(text='Назад', state='*')
+async def back_button_commands(message: types.Message):
     await message.reply('Выберите какой чай вы хотите приобрести', reply_markup=start_up_menu)
 
-@dp.message_handler(text=['Черный чай'])
+
+@dp.message_handler(text=['Новости и Акции'])
+async def avto_blogs(message: types.Message):
+    await message.answer('Текущие товары по акции:') #, reply_markup=sales_markup()
+
+@dp.message_handler(text=['Черный чай'], state='*')
 async def black_tea_command(message: types.Message):
     await message.reply('Выберите производителя который вы любите', reply_markup=black_tea_menu)
 
+@dp.message_handler(text=['Richard Black'], state='*')
+async def richard_black(message: types.Message):
+    photo = types.InputFile('photo/Richard_blac.jpg')
+    await message.answer_photo(photo=photo)
+    await message.answer('Доступны упаковки по 25 пакетиков (100 рублей), \n'
+                         'по 100 пакетиков (400 рублей)')
+    await message.answer('Выбери из списка') #, reply_markup = item_markup(table='autocarp', admin_ind=0)
 
-@dp.message_handler(text=['Зеленый чай'])
+
+@dp.message_handler(text=['Зеленый чай'], state='*')
 async def green_tea_command(message: types.Message):
     await message.reply('Выберите производителя который вы любите', reply_markup=green_tea_menu)
 
